9ba3ec495d100b75f5a661dff3e7464a35e6b6fa
Евгений Рамазанов <variolos@gmail.com>
2021-11-26 18:36:55 +0500

    Implement search and filtring functionaliti

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 
-import AppHeader from "../app-header";
+import AppHeader from "../app-header";      // Экспортирует AppHeader из /app-header/index.js читается WebPack'ом
 import SearchPanel from "../search-panel";
 import TodoList from "../todo-list";
 import ItemStatusFilter from '../item-status-filter';
@@ -17,7 +17,9 @@ export default class App extends Component {
             this.createTodoItem('Drink Coffee'),
             this.createTodoItem('Make Awesome App'),
             this.createTodoItem('Have a lunch'),
-        ]
+        ],
+        term: '',
+        filter: 'all' // active, all, done
     }
 
     createTodoItem(label) {
@@ -86,21 +88,57 @@ export default class App extends Component {
         ]
     }
 
+    search(items, term) {
+        if (term.length === 0) {
+            return items;
+        }
+        return  items.filter((item) => {
+            return item.label
+                .toLowerCase()
+                .indexOf(term.toLowerCase()) > -1;
+        })
+    }
+
+    onSearchChange = (term) => {
+        this.setState({term});
+    }
+
+    filter(items, filter) {
+        switch (filter) {
+            case 'all':
+                return items;
+            case 'active':
+                return items.filter((item) => !item.done);
+            case 'done':
+                return items.filter((item) => item.done);
+            default:
+                return items;
+        }
+    }
+
+    onFilterChange = (filter) => {
+        this.setState({filter});
+    }
+
     render() {
 
-        const {todoData} = this.state;
+        const {todoData, term, filter} = this.state;
         const doneCount = todoData.filter((el) => el.done).length;
         const todoCount = todoData.length - doneCount;
+
+        const visibleItems = this.filter(this.search(todoData, term), filter);
 
         return (
             <div className='todo-app'>
                 <AppHeader toDo={todoCount} done={doneCount}/>
                 <div className='top-panel d-flex'>
-                    <SearchPanel/>
-                    <ItemStatusFilter />
+                    <SearchPanel onSearchChange={this.onSearchChange}/>
+                    <ItemStatusFilter
+                        filter={filter}
+                        onFilterChange={this.onFilterChange}/>
                 </div>
                 <TodoList
-                    todos={todoData}
+                    todos={visibleItems}
                     onDeleted={this.deleteItem}
                     onToggleImportant={this.onToggleImportant}
                     onToggleDone={this.onToggleDone}/>
